28f50d4a201193c707ef4bc4bd5c558d5f80b8d2
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-07-30 15:09:34 -0700

    added toastify (nicer looking alerts), fancy interface typescript stuff, welcome user with there username using js

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+import AddTransaction from "@/components/AddTransaction";
+const HomePage = async () => {
+  const user = await currentUser();
 
-const HomePage = () => {
+  if (!user) {
+    return <Guest />
+  }
   return (
     <main>
-      <h1>Expense Tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
+      <AddTransaction />
     </main>
   );
 }
---
+++ components/AddTransaction.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import addTransaction from "@/app/actions/addTransaction";
+import {toast} from 'react-toastify';
+
+const AddTransaction = () => {
+    const clientAction = async (formData: FormData) => {
+        const {data, error } = await addTransaction(formData);
+
+        if (error) {
+            toast.error(error);
+        } else {
+            console.log(data);
+        }
+    };
+
+    return ( 
+        <>
+          <h3>Add transaction</h3>
+          <form action={clientAction}>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" id="text" name="text" placeholder="Enter text..." />
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
+                <input 
+                type="amount" 
+                id="amount" 
+                name="amount" 
+                placeholder="Enter amount..." 
+                step='0.01'
+                />
+            </div>
+            <button className="btn">Add transaction</button>
+          </form>
+        </>
+     );
+};
+ 
+export default AddTransaction;
