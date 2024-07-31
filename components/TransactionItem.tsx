a05f77e010cf885fdeb493ceb16fdd464a00239b
Bryan Trujillo <trujillobryan99@yahoo.com>
2024-07-31 14:51:54 -0700

    sign function

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -4,10 +4,13 @@ import { toast } from "react-toastify";
 
 const TransactionItem = ({ transaction }: { transaction:
     Transaction }) => {
+
+        const sign = transaction.amount < 0 ? '-' : '+';
+
         return (
             <li className={transaction.amount < 0 ? 'minus' : 'plus'}>{transaction.text}
             <span>
-                {addCommas(Math.abs(transaction.amount))}
+                {sign}${addCommas(Math.abs(transaction.amount))}
             </span>
             </li>
         )
